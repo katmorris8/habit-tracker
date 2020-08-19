f278aaefe9e4eb25ede0e605364f22b12f3e5c70
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-08-18 21:39:22 -0400

    remove the count function and only render the button. passed the props down from habit

--- src/HabitCountButton/index.js
+++ src/HabitCountButton/index.js
@@ -1,33 +1,12 @@
-// import React, { Component } from "react";
-// import "./style.css";
+import React, { Component } from "react";
+import "./style.css";
 
-// class HabitCountButton extends Component {
-//   constructor(props) {
-//     super(props)
-  
-//     this.state = {
-//       count: 0,
-//       counter: JSON.parse(localStorage.getItem('count')) || 0,
-//     }
-//   }
+class HabitCountButton extends Component {  
+  render () {
+    return (
+      <button className='counter-btn' onClick={this.props.click}>{this.props.count}</button>
+    )
+  }
+}
 
-//   updateHabitCounter = () => {
-//     this.setState(prevState => {
-//       let count = Number(prevState.count);
-//       let newCount = count + 1;
-//       localStorage.setItem('count', JSON.stringify(newCount));
-//       return ({
-//         count: newCount,
-//         counter: count,
-//       })
-//     })
-//   }
-  
-//   render () {
-//     return (
-//       <button className='counter-btn' onClick={this.updateHabitCounter}>{this.state.count}</button>
-//     )
-//   }
-// }
-
-// export default HabitCountButton;
+export default HabitCountButton;

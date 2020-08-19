beeebda508152ea9ddc91c06e2d28ae49293f7c5
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-08-19 08:51:08 -0400

    update habit props

--- src/Habits/index.js
+++ src/Habits/index.js
@@ -4,16 +4,22 @@ import Habit from "../Habit";
 import DeleteButton from "../DeleteButton";
 
 class Habits extends Component {
-
   render() {
     return (
       <div>
         <ul className='habits-list'>
         {this.props.habits.map((habit, index) => {
           return (
-            <li key={habit[index]} className='habit' >
-              <Habit key={habit} habits={habit} />
-              <DeleteButton className='delete-habit-btn' deleteHabit={()=> this.props.deleteHabit(index)} />
+            <li key={`${habit.text} ${index}`} className='habit' >
+              <Habit 
+                key={habit.text} 
+                habits={habit.text} 
+                count={habit.count} 
+              />
+              <DeleteButton 
+                className='delete-habit-btn' 
+                deleteHabit={()=> this.props.deleteHabit(index)} 
+              />
             </li>
           )
         })}

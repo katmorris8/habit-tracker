d852d234d7c55d9c5baac98316e8b7ad65266f5d
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-08-18 21:24:49 -0400

    add counter button and styles to habit coomponent

--- src/Habit/index.js
+++ src/Habit/index.js
@@ -2,11 +2,34 @@ import React, { Component } from "react";
 import "./style.css";
 
 class Habit extends Component {
+  constructor(props) {
+    super(props)
 
-  render () {
+    this.state = {
+      count: 0,
+      counter: JSON.parse(localStorage.getItem('count')) || 0,
+    }
+  }
+
+  updateHabitCounter = () => {
+    this.setState(prevState => {
+      let count = Number(prevState.count);
+      let newCount = count + 1;
+      localStorage.setItem('count', JSON.stringify(newCount));
+      return ({
+        count: newCount,
+        counter: count,
+      })
+    })
+  }
+
+  render() {
     const habit = this.props.habits;
     return (
-      <p className='habit-name' >{habit}</p>
+      <div>
+        <button className='counter-btn' onClick={this.updateHabitCounter}>{this.state.count}</button>
+        <p className='habit-name'>{habit}</p>
+      </div>
     )
   }
 }
--- src/HabitCountButton/index.js
+++ src/HabitCountButton/index.js
@@ -1,34 +1,33 @@
-import React, { Component } from "react";
-import "./style.css";
+// import React, { Component } from "react";
+// import "./style.css";
 
-class HabitCountButton extends Component {
-  constructor(props) {
-    super(props)
+// class HabitCountButton extends Component {
+//   constructor(props) {
+//     super(props)
   
-    this.state = {
-      count: 0,
-      counter: JSON.parse(localStorage.getItem('count')) || 0,
-    }
-  }
+//     this.state = {
+//       count: 0,
+//       counter: JSON.parse(localStorage.getItem('count')) || 0,
+//     }
+//   }
 
-  updateHabitCounter = () => {
-    this.setState(prevState => {
-      let count = Number(prevState.count);
-      let newCount = count + 1;
-      localStorage.setItem('count', JSON.stringify(newCount));
-      return ({
-        count: newCount,
-        counter: count,
-      })
-    })
-  }
+//   updateHabitCounter = () => {
+//     this.setState(prevState => {
+//       let count = Number(prevState.count);
+//       let newCount = count + 1;
+//       localStorage.setItem('count', JSON.stringify(newCount));
+//       return ({
+//         count: newCount,
+//         counter: count,
+//       })
+//     })
+//   }
   
-  render () {
-    const count = this.props.count;
-    return (
-      <button className='counter-btn' onClick={this.updateHabitCounter}>{this.state.count}</button>
-    )
-  }
-}
+//   render () {
+//     return (
+//       <button className='counter-btn' onClick={this.updateHabitCounter}>{this.state.count}</button>
+//     )
+//   }
+// }
 
-export default HabitCountButton;
+// export default HabitCountButton;

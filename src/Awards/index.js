8d0a77d9f18175c7905b8fe23c0f0c9e639c0019
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-08-25 22:44:15 -0400

    render awards conditionally

--- src/Awards/index.js
+++ src/Awards/index.js
@@ -7,17 +7,24 @@ class Awards extends Component {
     return (
       <div className='awards' >
         {this.props.habits.map((habit, index) => {
-          return (
-            <div key={`${habit.text} ${index}`} className='award' >
-              <div className='star' >
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                  <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-                </svg>
+            return (
+              <div>
+                {habit.count >= 21 && (
+                  <div key={`${habit.text} ${index} award`} className='award'>
+                    <div className='star' >
+                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
+                      </svg>
+                    </div>
+                    <p>{habit.text}</p>
+                  </div>
+                )}
+                {habit.count < 21 && (
+                  <p>It takes 21 days to form a habit. Come back when you've completed a habit at least 21 times!</p>
+                )}
               </div>
-              <p>{habit.text}</p>
-            </div>
-          )
-        })}
+            )
+          })}
       </div>
     )
   }

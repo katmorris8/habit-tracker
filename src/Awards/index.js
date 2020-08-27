29f85996c8427e5bf0f25ef3f4d6d95675d37b71
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-08-26 22:39:16 -0400

    added a key to the awards

--- src/Awards/index.js
+++ src/Awards/index.js
@@ -5,7 +5,7 @@ class Awards extends Component {
   constructor() {
     super();
     this.state = {
-      hasAwards: false
+      hasAwards: true
     }
   }
 
@@ -18,9 +18,9 @@ class Awards extends Component {
       <div className='awards' >
         {this.state.hasAwards && this.props.habits.map((habit, index) => {
             return (
-              <div>
+              <div className='award' key={`${habit.text} ${index} award`}>
                 {habit.count >= 21 && (
-                  <div key={`${habit.text} ${index} award`} className='award'>
+                  <div>
                     <div className='star' >
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />

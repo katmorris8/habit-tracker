28124bd230bf19736433bb9181d5b026e6a91076
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-09-04 22:43:13 -0400

    fix the conditional rendering to check the awarded status in the habit object

--- src/Awards/index.js
+++ src/Awards/index.js
@@ -3,14 +3,16 @@ import "./style.css";
 
 class Awards extends Component {
   render() {
+    const awardedArr = this.props.habits.map(habit => habit.awarded)
+    
     return (
       <div className='awards' >
-        {this.props.hasAwards && this.props.habits.map((habit, index) => {
+        {awardedArr.some(award => award) && this.props.habits.map((habit, index) => {
             return (
               <div className='award' key={`${habit.text} ${index} award`}>
-                {habit.count >= 21 && (
+                {habit.awarded && (
                   <div>
-                    <div className='star' >
+                    <div className='star'>
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                         <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
                       </svg>
@@ -21,7 +23,7 @@ class Awards extends Component {
               </div>
             )
           })}
-          {!this.props.hasAwards && (
+          {awardedArr.every(award => !award) && (
             <p>It takes 21 days to form a habit. Come back when you've completed a habit at least 21 times!</p>
           )}
       </div>

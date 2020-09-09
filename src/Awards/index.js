34059738ca9db320f840274b102ad5732f5eb92f
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-09-08 21:52:55 -0400

    render the award component in awards

--- src/Awards/index.js
+++ src/Awards/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./style.css";
+import Award from '../Award';
 
 class Awards extends Component {
   render() {
@@ -8,20 +9,11 @@ class Awards extends Component {
     return (
       <div className='awards' >
         {awardedArr.some(award => award) && this.props.habits.map((habit, index) => {
-            return (
-              <div className='award' key={`${habit.text} ${index} award`}>
-                {habit.awarded && (
-                  <div>
-                    <div className='star'>
-                      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                        <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-                      </svg>
-                    </div>
-                    <p>{habit.text}</p>
-                  </div>
-                )}
-              </div>
-            )
+          return(
+            <li key={`${habit.text} ${index} award`}>
+              <Award habit={habit} />
+            </li>
+          )
           })}
           {awardedArr.every(award => !award) && (
             <p>It takes 21 days to form a habit. Come back when you've completed a habit at least 21 times!</p>

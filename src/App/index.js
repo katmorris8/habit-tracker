f09dbd0ed50010032639fff82904363b51bd7885
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-09-13 21:00:35 -0400

    install and implement react dom confetti when habit count reaches 21

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import Confetti from 'react-dom-confetti';
 import "./style.css";
 import Nav from "../Nav";
 import HabitInput from "../HabitInput";
 import Habits from "../Habits";
 import Awards from "../Awards";
+
+const confettiConfig = {
+  angle: 90,
+  spread: 360,
+  startVelocity: 40,
+  elementCount: 70,
+  dragFriction: 0.12,
+  duration: 3000,
+  stagger: 3,
+  width: "10px",
+  height: "10px",
+  perspective: "500px",
+  colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+};
 
 class App extends Component {
   constructor(props) {
@@ -13,6 +28,7 @@ class App extends Component {
       habitInput: '',
       habits: JSON.parse(localStorage.getItem('habits')) || [],
       currentPanel: 'habits',
+      confetti: false
     }
   }
 
@@ -47,16 +63,23 @@ class App extends Component {
     })
   }
 
+  activateConfetti = () => {
+    this.setState({
+      confetti: true
+    })
+  }
+
   updateHabitCounter = index => {
     this.setState(prevState => {
       const habitList = [...prevState.habits];
       habitList[index].count += 1;
-      if (habitList[index].count >= 21) {
+      if (habitList[index].count === 21) {
         habitList[index].awarded = true;
+        this.activateConfetti();
       }
       localStorage.setItem('habits', JSON.stringify(habitList));
       return ({
-        habits: habitList
+        habits: habitList,
       })
     })
   }
@@ -92,6 +115,7 @@ class App extends Component {
                 counter={this.updateHabitCounter}
                 deleteHabit={this.deleteHabit}
               />
+              <Confetti active={ this.state.confetti } config={ confettiConfig }/>
             </div>
           )}
           {this.state.currentPanel === 'awards' && (

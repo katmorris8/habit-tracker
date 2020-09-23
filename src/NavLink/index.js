a4e01e8466baa4106cbbc9f995ac008559fea3b7
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-09-22 21:34:53 -0400

    add animate class name if the animate prop is true

--- src/NavLink/index.js
+++ src/NavLink/index.js
@@ -4,26 +4,13 @@ import "./style.css";
 class NavLink extends Component {
 
   render() {
-    // let habitsClassName = 'nav-link';
-    // let awardsClassName = 'nav-link awards-tab';
 
-    // if (this.props.panel === 'habits') {
-    //   habitsClassName += ' tab';
-    //   awardsClassName += ' not-tab';
+    // let btnClassName = 'nav-link';
+    // if (this.props.pannel === this.props.name) {
+    //   btnClassName += ' tab';
     // } else {
-    //   habitsClassName += ' not-tab';
-    //   awardsClassName += ' tab';
+    //   btnClassName += ' not-tab'; 
     // }
-
-    // let shakeClassName = 'shake';
-    // if (this.props.confetti) {
-    //   shakeClassName += ' shake-bottom';
-    // }
-    
-    let pClassName = '';
-    if (this.props.confetti && this.props.name === 'awards') {
-      pClassName += 'shake-bottom';
-    }
 
     return (
       <button className='nav-link' onClick={
@@ -33,7 +20,7 @@ class NavLink extends Component {
         }
       }
       >
-        <p className={pClassName}>{this.props.name}</p>
+        <p className={this.props.animate ? 'shake-bottom' : ''}>{this.props.name}</p>
       </button>
     )
   }

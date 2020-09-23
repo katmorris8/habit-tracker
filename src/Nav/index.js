e5d308eecfa74f5cd95ea1c1cdd1368bc19c68fb
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-09-22 20:25:16 -0400

    rendered the nav link instances and passed down the name and set panel props

--- src/Nav/index.js
+++ src/Nav/index.js
@@ -1,45 +1,36 @@
 import React, { Component } from "react";
 import "./style.css";
+import NavLink from '../NavLink';
 
 class Nav extends Component {
 
   render() {
-    let habitsClassName = 'nav-link';
-    let awardsClassName = 'nav-link awards-tab';
+    // let habitsClassName = 'nav-link';
+    // let awardsClassName = 'nav-link awards-tab';
 
-    if (this.props.panel === 'habits') {
-      habitsClassName += ' tab';
-      awardsClassName += ' not-tab';
-    } else {
-      habitsClassName += ' not-tab';
-      awardsClassName += ' tab';
-    }
+    // if (this.props.panel === 'habits') {
+    //   habitsClassName += ' tab';
+    //   awardsClassName += ' not-tab';
+    // } else {
+    //   habitsClassName += ' not-tab';
+    //   awardsClassName += ' tab';
+    // }
 
-    let shakeClassName = 'shake';
-    if (this.props.confetti) {
-      shakeClassName += ' shake-bottom';
-    }
+    // let shakeClassName = 'shake';
+    // if (this.props.confetti) {
+    //   shakeClassName += ' shake-bottom';
+    // }
 
     return (
       <nav className='nav-bar' >
-        <button className={habitsClassName} onClick={
-          event => {
-            event.preventDefault();
-            this.props.setPanel('habits');
-          }
-        }
-        >
-          Habits
-        </button>
-        <button className={awardsClassName} href="#" onClick={
-          event => {
-            event.preventDefault();
-            this.props.setPanel('awards');
-          }
-        }
-        >
-          <p className={shakeClassName}>Awards</p>
-        </button>
+        <NavLink 
+          name={'habits'}
+          setPanel={this.props.setPanel}
+        />
+        <NavLink
+          name={'awards'}
+          setPanel={this.props.setPanel}
+        />
       </nav>
     )
   }

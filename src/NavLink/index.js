716cbd1e0df3186b7dbc342fe42a7b114aefd09c
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-09-22 20:24:53 -0400

    create nav link button component

---
+++ src/NavLink/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class NavLink extends Component {
+
+  render() {
+    // let habitsClassName = 'nav-link';
+    // let awardsClassName = 'nav-link awards-tab';
+
+    // if (this.props.panel === 'habits') {
+    //   habitsClassName += ' tab';
+    //   awardsClassName += ' not-tab';
+    // } else {
+    //   habitsClassName += ' not-tab';
+    //   awardsClassName += ' tab';
+    // }
+
+    // let shakeClassName = 'shake';
+    // if (this.props.confetti) {
+    //   shakeClassName += ' shake-bottom';
+    // }
+
+    return (
+      <button  href="#" onClick={
+        event => {
+          event.preventDefault();
+          this.props.setPanel(this.props.name);
+        }
+      }
+      >
+        <p >{this.props.name}</p>
+      </button>
+    )
+  }
+}
+
+export default NavLink;

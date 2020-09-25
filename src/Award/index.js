5bd784b1fd37a3f49d00506aa7341d8379c9fcc9
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-09-25 14:57:38 -0400

    center award name in the star

--- src/Award/index.js
+++ src/Award/index.js
@@ -6,13 +6,13 @@ class Award extends Component {
     return (
       <div>
         {this.props.habit.awarded && (
-          <div>
+          <div className="award-container">
             <div className='star'>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                 <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
               </svg>
             </div>
-            <p>{this.props.habit.text}</p>
+            <p className="award-name">{this.props.habit.text}</p>
           </div>
         )}
       </div>

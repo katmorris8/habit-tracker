4ed8aac73b676f10b0c6d625cd6891bc419a6b6e
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-10-09 16:37:34 -0400

    add confetti instance

--- src/HabitCountButton/index.js
+++ src/HabitCountButton/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./style.css";
+import Confetti from 'react-dom-confetti';
 
 class HabitCountButton extends Component {  
   render () {
     return (
       <button className='counter-btn' onClick={this.props.click}>
         {this.props.count}
+        <Confetti active={ this.props.confetti } config={ this.props.confettiConfig }/>
       </button>
     )
   }

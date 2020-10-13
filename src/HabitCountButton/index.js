207862a129d9cb58db449f6cfe8f2cfdfa9763cb
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-10-12 20:39:29 -0400

    style background color using the color prop

--- src/HabitCountButton/index.js
+++ src/HabitCountButton/index.js
@@ -4,8 +4,11 @@ import Confetti from 'react-dom-confetti';
 
 class HabitCountButton extends Component {  
   render () {
+    const style = {
+      backgroundColor: this.props.color
+    }
     return (
-      <button className='counter-btn' onClick={this.props.click}>
+      <button className='counter-btn' style={style} onClick={this.props.click}>
         {this.props.count}
         <Confetti active={ this.props.confetti } config={ this.props.confettiConfig }/>
       </button>

219b976876005a48f617b5d692d117503e2ad955
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-10-13 20:19:52 -0400

    update favicon

--- src/App/index.js
+++ src/App/index.js
@@ -19,7 +19,7 @@ const confettiConfig = {
   colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
 };
 
-let colorIndex = -1;
+// let colorIndex = -1;
 
 const storedHabits = (item) => JSON.parse(localStorage.getItem(item));
 
@@ -33,7 +33,8 @@ class App extends Component {
       habitInput: '',
       habits: storedHabits('habits') || [],
       currentPanel: 'habits',
-      confetti: false
+      confetti: false,
+      colorIndex: -1
     }
   }
 
@@ -106,21 +107,32 @@ class App extends Component {
   }
 
   habitColors = () => {
-    colorIndex++;
-    if (colorIndex >= confettiConfig.colors.length) {
-      colorIndex = 0;
-    }
-    return confettiConfig.colors[colorIndex];
+    this.setState(prevState => {
+      let colorIndex = prevState.colorIndex;
+      colorIndex++;
+      if (colorIndex >= confettiConfig.colors.length) {
+        colorIndex = 0;
+      }
+      return ({
+        colorIndex
+      })
+    })
+    // return confettiConfig.colors[colorIndex];
   }
 
   setHabitColor = () => {
     this.setState(prevState => {
       const habitList = [...prevState.habits];
-      habitList.forEach(habit => habit.color = this.habitColors());
+      habitList.forEach(habit => {
+        this.habitColors();
+        const index = prevState.colorIndex;
+        habit.color = confettiConfig.colors[index];
+      });
       setLocalStorage('habits', habitList);
-      colorIndex = -1;
+      // colorIndex = -1;
       return ({
-        habits: habitList
+        habits: habitList,
+        colorIndex: -1
       })
     })
   }

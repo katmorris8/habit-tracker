17b54712fc016b8c47bb66014c0acae492759ff4
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-10-14 21:21:38 -0400

    add conditional rendering for when there are no habits to render

--- src/Habits/index.js
+++ src/Habits/index.js
@@ -2,35 +2,42 @@ import React, { Component } from "react";
 import "./style.css";
 import Habit from "../Habit";
 import DeleteButton from "../DeleteButton";
+import arrow from '../images/arrow.gif'
 
 class Habits extends Component {
   render() {
     return (
       <div>
         <ul className='habits-list'>
-        {this.props.habits.map((habit, index) => {
-          const style = {
-            backgroundColor: `${habit.color}60`
-          }
-          return (
-            <li key={`${habit.text} ${index}`} className='habit' style={style} >
-              <Habit 
-                key={habit.text}
-                id={index} 
-                habits={habit.text}
-                color={habit.color} 
-                count={habit.count} 
-                counter={() => this.props.counter(index)}
-                confetti={this.props.confetti}
-                confettiConfig={this.props.confettiConfig}
-              />
-              <DeleteButton 
-                className='delete-habit-btn' 
-                deleteHabit={() => this.props.deleteHabit(index)} 
-              />
-            </li>
-          )
-        })}
+          {this.props.habits.length > 0 && this.props.habits.map((habit, index) => {
+            const style = {
+              backgroundColor: `${habit.color}60`
+            }
+            return (
+              <li key={`${habit.text} ${index}`} className='habit' style={style} >
+                <Habit
+                  key={habit.text}
+                  id={index}
+                  habits={habit.text}
+                  color={habit.color}
+                  count={habit.count}
+                  counter={() => this.props.counter(index)}
+                  confetti={this.props.confetti}
+                  confettiConfig={this.props.confettiConfig}
+                />
+                <DeleteButton
+                  className='delete-habit-btn'
+                  deleteHabit={() => this.props.deleteHabit(index)}
+                />
+              </li>
+            )
+          })}
+          {this.props.habits.length === 0 && (
+            <div className='empty-habits-message'>
+              <p>create a new habit using the button below!</p>
+              <img src={arrow} alt='animated arrow' />
+            </div>
+          )}
         </ul>
       </div>
     )

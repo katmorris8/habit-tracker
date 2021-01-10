072b849df85b08614bbd87f39ff514757c12a7ff
katmorris8 <kathryn.leigh.morris@gmail.com>
2021-01-10 16:18:24 -0500

    update the nav button click. remove unnecessary event parameter and the prevent default

--- src/NavLink/index.js
+++ src/NavLink/index.js
@@ -5,12 +5,7 @@ class NavLink extends Component {
 
   render() {
     return (
-      <button className='nav-link' onClick={
-        event => {
-          event.preventDefault();
-          this.props.setPanel(this.props.name);
-        }
-      }>
+      <button className='nav-link' onClick={() => this.props.setPanel(this.props.name)}>
         <p className={`${this.props.animate ? 'shake-bottom' : ''} ${this.props.className}`}>
           <i className={this.props.icon}></i>
           <br></br>
